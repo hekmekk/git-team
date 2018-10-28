12ba1cab295b519c0648cf10796110f2dcf37038
Rea Sand <hekmek@posteo.de>
2018-10-28 12:37:32 +0100

    refactor: Return output lines from git exec func
    also:
    Add proper output for alias management commands

--- core/handler/add.go
+++ core/handler/add.go
@@ -2,11 +2,19 @@ package handler
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/fatih/color"
 	"github.com/hekmekk/git-team/core/git"
 )
 
 func AddCommand(alias, coauthor *string) {
+	_, resolveErr := git.ResolveAlias(*alias)
+	if resolveErr == nil {
+		color.Yellow(fmt.Sprintf("Alias '%s' has already been added.", *alias))
+		os.Exit(0)
+	}
+
 	checkErr := git.SanityCheckCoauthor(*coauthor)
 	if checkErr != nil {
 		ToStderrAndExit(checkErr)
@@ -15,5 +23,5 @@ func AddCommand(alias, coauthor *string) {
 	if addErr != nil {
 		ToStderrAndExit(addErr)
 	}
-	color.Green(fmt.Sprintf("Alias '%s' -> %s has been added.", *alias, *coauthor))
+	color.Green(fmt.Sprintf("Alias '%s' -> '%s' has been added.", *alias, *coauthor))
 }
