0e4eea4ee23e470fa376e50f77d5f5083c51676c
Rea Sand <hekmek@posteo.de>
2019-07-05 13:53:51 +0200

    refactor: Make add command idempotent

--- core/handler/add.go
+++ core/handler/add.go
@@ -1,28 +1,16 @@
 package handler
 
-import (
-	"errors"
-	"fmt"
-
-	"github.com/fatih/color"
-)
-
 type AliasAdded struct {
 	Alias    string
 	CoAuthor string
 }
 
-func RunAddCommand(resolve func(string) (string, error), add func(string, string) error) func(string, string) (AliasAdded, error, int) {
-	return func(alias string, coauthor string) (AliasAdded, error, int) {
-		_, resolveErr := resolve(alias)
-		if resolveErr == nil {
-			return AliasAdded{}, errors.New(color.YellowString(fmt.Sprintf("Alias '%s' has already been added.", alias))), 0
-		}
-
+func RunAddCommand(add func(string, string) error) func(string, string) (AliasAdded, error) {
+	return func(alias string, coauthor string) (AliasAdded, error) {
 		addErr := add(alias, coauthor)
 		if addErr != nil {
-			return AliasAdded{}, addErr, -1
+			return AliasAdded{}, addErr
 		}
-		return AliasAdded{Alias: alias, CoAuthor: coauthor}, nil, 0
+		return AliasAdded{Alias: alias, CoAuthor: coauthor}, nil
 	}
 }
--- core/handler/add_test.go
+++ core/handler/add_test.go
@@ -9,24 +9,15 @@ func TestAddSucceeds(t *testing.T) {
 	alias := "mr"
 	coAuthor := "Mr. Noujz <noujz@mr.se>"
 
-	resolve := func(alias string) (string, error) {
-		return "", errors.New("No alias found")
-	}
-
 	add := func(alias, coAuthor string) error {
 		return nil
 	}
 
-	handleAdd := RunAddCommand(resolve, add)
-	aliasAdded, err, exitCode := handleAdd(alias, coAuthor)
+	handleAdd := RunAddCommand(add)
+	aliasAdded, err := handleAdd(alias, coAuthor)
 
 	if err != nil {
 		t.Error(err)
-		t.Fail()
-	}
-
-	if exitCode != 0 {
-		t.Error("Exit Code != 0")
 		t.Fail()
 	}
 
@@ -38,59 +29,18 @@ func TestAddSucceeds(t *testing.T) {
 	}
 }
 
-func TestAddFailsBecauseAliasIsDefined(t *testing.T) {
-	alias := "mr"
-	coAuthor := "Mr. Noujz <noujz@mr.se>"
-
-	resolve := func(alias string) (string, error) {
-		return coAuthor, nil
-	}
-
-	add := func(alias, coAuthor string) error {
-		return nil
-	}
-
-	handleAdd := RunAddCommand(resolve, add)
-	aliasAdded, err, exitCode := handleAdd(alias, coAuthor)
-
-	if err == nil {
-		t.Fail()
-	}
-
-	if exitCode != 0 {
-		t.Error("Exit Code != 0")
-		t.Fail()
-	}
-
-	expected := AliasAdded{}
-
-	if aliasAdded != expected {
-		t.Errorf("was: %s, expected: %s", aliasAdded, expected)
-		t.Fail()
-	}
-}
-
 func TestAddFailsBecauseUnderlyingGitAddFails(t *testing.T) {
 	alias := "mr"
 	coAuthor := "Mr. Noujz <noujz@mr.se>"
-
-	resolve := func(alias string) (string, error) {
-		return "", errors.New("No alias found")
-	}
 
 	add := func(alias, coAuthor string) error {
 		return errors.New("git add failed")
 	}
 
-	handleAdd := RunAddCommand(resolve, add)
-	aliasAdded, err, exitCode := handleAdd(alias, coAuthor)
+	handleAdd := RunAddCommand(add)
+	aliasAdded, err := handleAdd(alias, coAuthor)
 
 	if err == nil {
-		t.Fail()
-	}
-
-	if exitCode != -1 {
-		t.Error("Exit Code != -1")
 		t.Fail()
 	}
 
