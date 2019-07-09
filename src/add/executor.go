90d44785d12f8807897e71ce29f2e17e28d36c2a
Rea Sand <hekmek@posteo.de>
2019-07-09 17:51:29 +0200

    docs: Add TODO on add command

--- src/add/executor.go
+++ src/add/executor.go
@@ -5,6 +5,7 @@ type AliasAdded struct {
 	CoAuthor string
 }
 
+// TODO: Refactor to match enable command pattern (don't use effects type tho, we have only one...)
 func RunAddCommand(add func(string, string) error) func(string, string) (AliasAdded, error) {
 	return func(alias string, coauthor string) (AliasAdded, error) {
 		addErr := add(alias, coauthor)
