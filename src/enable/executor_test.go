b4c2062f68e30133b6c05b12ce2b1d652c8fc0f1
Rea Sand <hekmek@posteo.de>
2019-07-11 17:31:32 +0200

    test: Finalize suite for enable command

--- src/enable/executor_test.go
+++ src/enable/executor_test.go
@@ -1,6 +1,7 @@
 package enable
 
 import (
+	"errors"
 	"os"
 	"testing"
 
@@ -34,6 +35,142 @@ func TestEnableSucceeds(t *testing.T) {
 	err := execEnable(cmd)
 
 	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestEnableFailsDueToCreateDirErr(t *testing.T) {
+	coAuthors := []string{"Mr. Noujz <noujz@mr.se>"}
+	baseDir := "BASEDIR"
+	templateFileName := "TEMPLATE_FILE"
+
+	expectedErr := errors.New("Failed to create Dir")
+
+	createDir := func(string, os.FileMode) error { return expectedErr }
+	writeFile := func(string, []byte, os.FileMode) error { return nil }
+	setCommitTemplate := func(string) error { return nil }
+	saveStatus := func(status.State, ...string) error { return nil }
+
+	effect := EnableEffect{
+		CreateDir:         createDir,
+		WriteFile:         writeFile,
+		SetCommitTemplate: setCommitTemplate,
+		SaveStatus:        saveStatus,
+	}
+	execEnable := ExecutorFactory(effect)
+
+	cmd := EnableCommand{
+		Coauthors:        coAuthors,
+		BaseDir:          baseDir,
+		TemplateFileName: templateFileName,
+	}
+
+	err := execEnable(cmd)
+
+	if err != expectedErr {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestEnableFailsDueToWriteFileErr(t *testing.T) {
+	coAuthors := []string{"Mr. Noujz <noujz@mr.se>"}
+	baseDir := "BASEDIR"
+	templateFileName := "TEMPLATE_FILE"
+
+	expectedErr := errors.New("Failed to write file")
+
+	createDir := func(string, os.FileMode) error { return nil }
+	writeFile := func(string, []byte, os.FileMode) error { return expectedErr }
+	setCommitTemplate := func(string) error { return nil }
+	saveStatus := func(status.State, ...string) error { return nil }
+
+	effect := EnableEffect{
+		CreateDir:         createDir,
+		WriteFile:         writeFile,
+		SetCommitTemplate: setCommitTemplate,
+		SaveStatus:        saveStatus,
+	}
+	execEnable := ExecutorFactory(effect)
+
+	cmd := EnableCommand{
+		Coauthors:        coAuthors,
+		BaseDir:          baseDir,
+		TemplateFileName: templateFileName,
+	}
+
+	err := execEnable(cmd)
+
+	if err != expectedErr {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestEnableFailsDueToSetCommitTemplateErr(t *testing.T) {
+	coAuthors := []string{"Mr. Noujz <noujz@mr.se>"}
+	baseDir := "BASEDIR"
+	templateFileName := "TEMPLATE_FILE"
+
+	expectedErr := errors.New("Failed to set commit template")
+
+	createDir := func(string, os.FileMode) error { return nil }
+	writeFile := func(string, []byte, os.FileMode) error { return nil }
+	setCommitTemplate := func(string) error { return expectedErr }
+	saveStatus := func(status.State, ...string) error { return nil }
+
+	effect := EnableEffect{
+		CreateDir:         createDir,
+		WriteFile:         writeFile,
+		SetCommitTemplate: setCommitTemplate,
+		SaveStatus:        saveStatus,
+	}
+	execEnable := ExecutorFactory(effect)
+
+	cmd := EnableCommand{
+		Coauthors:        coAuthors,
+		BaseDir:          baseDir,
+		TemplateFileName: templateFileName,
+	}
+
+	err := execEnable(cmd)
+
+	if err != expectedErr {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestEnableFailsDueToSaveStatusErr(t *testing.T) {
+	coAuthors := []string{"Mr. Noujz <noujz@mr.se>"}
+	baseDir := "BASEDIR"
+	templateFileName := "TEMPLATE_FILE"
+
+	expectedErr := errors.New("Failed to set status")
+
+	createDir := func(string, os.FileMode) error { return nil }
+	writeFile := func(string, []byte, os.FileMode) error { return nil }
+	setCommitTemplate := func(string) error { return nil }
+	saveStatus := func(status.State, ...string) error { return expectedErr }
+
+	effect := EnableEffect{
+		CreateDir:         createDir,
+		WriteFile:         writeFile,
+		SetCommitTemplate: setCommitTemplate,
+		SaveStatus:        saveStatus,
+	}
+	execEnable := ExecutorFactory(effect)
+
+	cmd := EnableCommand{
+		Coauthors:        coAuthors,
+		BaseDir:          baseDir,
+		TemplateFileName: templateFileName,
+	}
+
+	err := execEnable(cmd)
+
+	if err != expectedErr {
 		t.Error(err)
 		t.Fail()
 	}
