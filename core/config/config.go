8c4a303167691f836246747fdac6f948d95e80fc
Rea Sand <hekmek@posteo.de>
2019-07-12 16:12:11 +0200

    test: Add tests for git-team configuration package

--- core/config/config.go
+++ core/config/config.go
@@ -4,17 +4,29 @@ import (
 	"github.com/mitchellh/go-homedir"
 )
 
+// Config currently static config for git-team
 type Config struct {
 	BaseDir          string
 	TemplateFileName string
 	StatusFileName   string
 }
 
+// Load loads the configuration file
 func Load() (Config, error) {
-	baseDir, err := homedir.Expand("~/.config/git-team")
-	if err != nil {
-		return Config{}, err
-	}
+	return executorFactory(dependencies{expandHomedir: homedir.Expand})()
+}
 
-	return Config{BaseDir: baseDir, TemplateFileName: "COMMIT_TEMPLATE", StatusFileName: "status.toml"}, nil
+type dependencies struct {
+	expandHomedir func(string) (string, error)
+}
+
+func executorFactory(deps dependencies) func() (Config, error) {
+	return func() (Config, error) {
+		baseDir, err := deps.expandHomedir("~/.config/git-team")
+		if err != nil {
+			return Config{}, err
+		}
+
+		return Config{BaseDir: baseDir, TemplateFileName: "COMMIT_TEMPLATE", StatusFileName: "status.toml"}, nil
+	}
 }
---
+++ core/config/config_test.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestLoadSucceeds(t *testing.T) {
+	expectedBaseDir := "BASEDIR"
+
+	deps := dependencies{expandHomedir: func(path string) (string, error) { return expectedBaseDir, nil }}
+
+	expectedCfg := Config{BaseDir: expectedBaseDir, TemplateFileName: "COMMIT_TEMPLATE", StatusFileName: "status.toml"}
+
+	cfg, err := executorFactory(deps)()
+
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+
+	if reflect.DeepEqual(expectedCfg, cfg) != true {
+		t.Errorf("expected: %s, received %s", expectedCfg, cfg)
+		t.Fail()
+	}
+}
+
+func TestLoadFailsBecauseHomeDirExpansionFails(t *testing.T) {
+	deps := dependencies{expandHomedir: func(path string) (string, error) { return "", errors.New("failed to expand dir") }}
+
+	expectedCfg := Config{}
+
+	cfg, err := executorFactory(deps)()
+
+	if err == nil {
+		t.Error("expected failure")
+		t.Fail()
+	}
+
+	if reflect.DeepEqual(expectedCfg, cfg) != true {
+		t.Errorf("expected: %s, received %s", expectedCfg, cfg)
+		t.Fail()
+	}
+}
