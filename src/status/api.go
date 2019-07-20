263732f2828eff26e9cc897d1ac096cb105b9537
Rea Sand <hekmek@posteo.de>
2019-07-21 01:03:43 +0200

    docs: Document the status api

--- src/status/api.go
+++ src/status/api.go
@@ -8,14 +8,17 @@ import (
 	"github.com/hekmekk/git-team/src/config"
 )
 
+// PersistEnabled persist the current git-team state as enabled
 func PersistEnabled(coauthors []string) error {
 	return persist(state{Status: enabled, Coauthors: coauthors})
 }
 
+// PersistDisabled persist the current git-team state as disabled
 func PersistDisabled() error {
 	return persist(state{Status: disabled})
 }
 
+// Print show the current state
 func Print() {
 	status := fetch()
 	fmt.Println(status.toString())
