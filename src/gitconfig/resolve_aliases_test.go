c045bcfa623da0323aae4a5c9369c2563b9e7fe0
Rea Sand <hekmek@posteo.de>
2019-07-26 14:52:32 +0200

    refactor: make resolveAliases testable and add tests

---
+++ src/gitconfig/resolve_aliases_test.go
@@ -0,0 +1,50 @@
+package gitconfig
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestShouldReturnNoErrors(t *testing.T) {
+	aliases := []string{"mrs", "mr"}
+	coauthorMapping := map[string]string{"mrs": "Mrs. Noujz <noujz@mrs.se>", "mr": "Mr. Noujz <noujz@mr.se>"}
+
+	var expectedCoauthors []string
+	for _, coauthor := range coauthorMapping {
+		expectedCoauthors = append(expectedCoauthors, coauthor)
+	}
+
+	resolveAlias := func(alias string) (string, error) { return coauthorMapping[alias], nil }
+
+	coauthors, errs := resolveAliases(resolveAlias)(aliases)
+
+	if len(errs) > 0 {
+		t.Errorf("unexpected errors: %s", errs)
+		t.Fail()
+	}
+
+	if !reflect.DeepEqual(expectedCoauthors, coauthors) {
+		t.Errorf("expected: %s, got: %s", expectedCoauthors, coauthors)
+		t.Fail()
+	}
+}
+
+type resolveresult struct {
+	coauthor string
+	err      error
+}
+
+func TestShouldAccumulateErrs(t *testing.T) {
+	aliases := []string{"mrs", "mr"}
+	coauthorMapping := map[string]resolveresult{"mrs": resolveresult{coauthor: "Mrs. Noujz <noujz@mrs.se>", err: nil}, "mr": resolveresult{coauthor: "", err: errors.New("failed to resolve alias mr")}}
+
+	resolveAlias := func(alias string) (string, error) { return coauthorMapping[alias].coauthor, coauthorMapping[alias].err }
+
+	_, errs := resolveAliases(resolveAlias)(aliases)
+
+	if len(errs) != 1 || errs[0].Error() != "failed to resolve alias mr" {
+		t.Errorf("unexpected amount of errors: %s", errs)
+		t.Fail()
+	}
+}
