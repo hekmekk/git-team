4aaff6ca0795e18610591a1508f2a5e86d80559a
hekmekk <hekmek@posteo.de>
2019-08-09 19:57:52 +0200

    chore: (WIP) refine implementation of 'release' target

--- scripts/release.lua
+++ scripts/release.lua
@@ -1,6 +1,17 @@
--- sudo luarocks install argparse luasec inspect
+--[[ Dependencies
+sudo apt install luarocks libssl-dev
+sudo luarocks install argparse
+sudo luarocks install luasec
+sudo luarocks install inspect
+sudo luarocks install lua-cjson
+sudo luarocks install sha2
+--]]
 
--- # TODO: always update asset and body with checksum together
+--[[ Purge a release (and respective tag)
+curl -v -f -H 'Authorization: token GITHUB_API_TOKEN' -X DELETE https://api.github.com/repos/hekmekk/git-team/releases/:id
+git tag -d VERSION
+git push origin --delete VERSION
+--]]
 
 local ltn12 = require "ltn12"
 local json = require "cjson"
@@ -52,23 +63,18 @@ function upload_asset(github_api_token, upload_url, deb_file)
   }
   respbody = table.concat(respbody)
 
-  if respcode == 201 then
-    print('[debug] successfully uploaded asset')
-    return nil
-  end
-
   if respbody and respbody ~= "" then
-    return json.decode(respbody)
+    return respcode, json.decode(respbody)
   end
 
-  return nil
+  return respcode, nil
 end
 
 function update_release(github_api_token, releases_uri, release_id, request_body)
   local respbody = {} -- for the response body
 
   local result, respcode, respheaders, respstatus = https.request {
-      method = 'PUT',
+      method = 'PATCH',
       url = releases_uri .. '/' .. release_id,
       source = ltn12.source.string(request_body),
       headers = {
@@ -88,8 +94,8 @@ function update_release(github_api_token, releases_uri, release_id, request_body
 end
 
 function create_release(releases_uri, github_api_token, version)
-  local reqbody = json.encode({ tag_name = version })
-  local respbody = {} -- for the response body
+  local reqbody = json.encode({ tag_name = version, body = '', name = version })
+  local respbody = {}
 
   local result, respcode, respheaders, respstatus = https.request {
       method = 'POST',
@@ -137,9 +143,7 @@ end
 function interactively_add_git_tag_and_push_to_remote(version)
   local x = os.execute(string.format('git tag -a %s', version))
   if 0 == x then
-    print(string.format('[debug] latest commit has been tagged', version))
     os.execute('git push origin --tags')
-    print(string.format('[debug] remote tags have been updated', version))
   end
 end
 
@@ -154,14 +158,14 @@ local releases_uri = 'https://api.github.com/repos/hekmekk/git-team/releases'
 
 local respcode, release = find_release_in_tags(releases_uri, git_team_version)
 if respcode == 200 then
-  print(string.format("[info] release found for version=%s", git_team_version))
+  print(string.format("[info ] release with id=%s found for version=%s", release['id'], git_team_version))
 end
 if respcode == 404 then
-  print(string.format("[info] no release found for version=%s", git_team_version))
-  print(string.format('[info] updating git tags', git_team_version))
+  print(string.format("[info ] no release found for version=%s", git_team_version))
   interactively_add_git_tag_and_push_to_remote(git_team_version)
-  print(string.format('[info] going to create release for version=%s', git_team_version))
-  _, release = create_release(releases_uri, github_api_token, git_team_version)
+  print(string.format('[info ] latest commit has been tagged', version))
+  print(string.format('[info ] remote tags have been updated', version))
+  local create_ret, release = create_release(releases_uri, github_api_token, git_team_version)
 end
 if respcode ~= 200 and respcode ~= 404 then
   print(string.format("[error] failure while trying to find release for version=%s", git_team_version))
@@ -177,12 +181,17 @@ for x in git_team_deb_path:gmatch("([^/]+)/?") do deb_file_name = x end
 
 local deb_file = read_file(git_team_deb_path)
 if not is_asset_uploaded_already(release['assets'], deb_file_name) then
-  print("[debug] asset uploaded already")
   local upload_url_template = release['upload_url']
   local upload_url = string.gsub(upload_url_template, "%{%?name,label%}", string.format('?name=%s', deb_file_name))
 
-  local resp = upload_asset(github_api_token, upload_url, deb_file)
-  print(resp)
+  local respcode, body = upload_asset(github_api_token, upload_url, deb_file)
+  if respcode == 201 then
+    print("[info ] asset uploaded successfully")
+  else
+    print("[error] failed to upload asset")
+    print(string.format("[debug] code=%s", respcode))
+    print(string.format("[debug] body=%s", inspect(body)))
+  end
 end
 
 local sha256sum = sha2.sha256hex(deb_file)
@@ -192,7 +201,15 @@ if not is_checksum_in_body_already(release, sha256sum) then
     body = string.format('**sha256 checksum:** `%s`', sha256sum)
   })
 
-  local res = update_release(github_api_token, releases_uri, release_id, reqbody)
-  print(res)
+  local respcode2, body2 = update_release(github_api_token, releases_uri, release_id, reqbody)
+
+  if respcode2 == 200 then
+    print("[info ] checksum added successfully")
+  else
+    print("[error] failed to update release with checksum")
+    print(string.format("[debug] release_id=%s", release_id))
+    print(string.format("[debug] code=%s", respcode2))
+    print(string.format("[debug] body=%s", inspect(body2)))
+  end
 end
 
