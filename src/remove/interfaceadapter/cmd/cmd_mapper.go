c65a933d7914ee7b0b57e4e94441d5323162b640
Rea Sand <hekmek@posteo.de>
2019-08-22 22:18:39 +0200

    refactor: implement rm in terms of events and effects

---
+++ src/remove/interfaceadapter/cmd/cmd_mapper.go
@@ -0,0 +1,32 @@
+package removecmdadapter
+
+import (
+	"gopkg.in/alecthomas/kingpin.v2"
+
+	"github.com/hekmekk/git-team/src/gitconfig"
+	"github.com/hekmekk/git-team/src/remove"
+)
+
+// Definition the command, arguments, and dependencies
+type Definition struct {
+	CommandName string
+	Request     remove.DeAllocationRequest
+	Deps        remove.Dependencies
+}
+
+// NewDefinition the constructor for Definition
+func NewDefinition(app *kingpin.Application) Definition {
+
+	command := app.Command("rm", "Remove an alias to co-author assignment")
+	alias := command.Arg("alias", "The alias to identify the assignment to be removed").Required().String()
+
+	return Definition{
+		CommandName: command.FullCommand(),
+		Request: remove.DeAllocationRequest{
+			Alias: alias,
+		},
+		Deps: remove.Dependencies{
+			GitRemoveAlias: gitconfig.RemoveAlias,
+		},
+	}
+}
---
+++ src/remove/policy_test.go
@@ -0,0 +1,60 @@
+package remove
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+)
+
+func TestRmShouldRemoveTheAssignment(t *testing.T) {
+	alias := "mr"
+
+	remove := func(alias string) error {
+		return nil
+	}
+
+	expectedEvent := DeAllocationSucceeded{Alias: alias}
+
+	event := Apply(Dependencies{GitRemoveAlias: remove}, DeAllocationRequest{Alias: &alias})
+
+	if !reflect.DeepEqual(expectedEvent, event) {
+		t.Errorf("expected: %s, got: %s", expectedEvent, event)
+		t.Fail()
+	}
+}
+
+func TestRmShouldRemoveTheAssignmentWhenTryingToRemoveANonExistingAlias(t *testing.T) {
+	alias := "mr"
+
+	remove := func(alias string) error {
+		return errors.New("exit status 5")
+	}
+
+	expectedEvent := DeAllocationSucceeded{Alias: alias}
+
+	event := Apply(Dependencies{GitRemoveAlias: remove}, DeAllocationRequest{Alias: &alias})
+
+	if !reflect.DeepEqual(expectedEvent, event) {
+		t.Errorf("expected: %s, got: %s", expectedEvent, event)
+		t.Fail()
+	}
+}
+
+func TestRmShouldFailBecauseUnderlyingGitRemoveFails(t *testing.T) {
+	alias := "mr"
+
+	err := errors.New("git remove DeAllocationRequest failed")
+
+	remove := func(alias string) error {
+		return err
+	}
+
+	expectedEvent := DeAllocationFailed{Reason: err}
+
+	event := Apply(Dependencies{GitRemoveAlias: remove}, DeAllocationRequest{Alias: &alias})
+
+	if !reflect.DeepEqual(expectedEvent, event) {
+		t.Errorf("expected: %s, got: %s", expectedEvent, event)
+		t.Fail()
+	}
+}
