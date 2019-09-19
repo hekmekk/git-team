34e32afe54add89baa8fa6cc137da7bb5f341074
Rea Sand <hekmek@posteo.de>
2019-09-19 16:40:46 +0200

    refactor: run disable command via kingpin.Action

--- src/command/disable/interfaceadapter/cmd/cmd_mapper.go
+++ src/command/disable/interfaceadapter/cmd/cmd_mapper.go
@@ -5,34 +5,32 @@ import (
 
 	"gopkg.in/alecthomas/kingpin.v2"
 
+	"github.com/hekmekk/git-team/src/command/adapter"
 	"github.com/hekmekk/git-team/src/command/disable"
+	"github.com/hekmekk/git-team/src/command/disable/interfaceadapter/event"
 	"github.com/hekmekk/git-team/src/core/config"
 	"github.com/hekmekk/git-team/src/core/gitconfig"
 	"github.com/hekmekk/git-team/src/core/state_repository"
 )
 
-// Definition definition of the disable command
-type Definition struct {
-	CommandName string
-	Policy      disable.Policy
+// Command the disable command
+func Command(root commandadapter.CommandRoot) *kingpin.CmdClause {
+	disable := root.Command("disable", "Use default commit template and remove prepare-commit-msg hook")
+
+	disable.Action(commandadapter.Run(policy(), disableeventadapter.MapEventToEffectsFactory(staterepository.Query)))
+
+	return disable
 }
 
-// NewDefinition the constructor for Definition
-func NewDefinition(app *kingpin.Application) Definition {
-
-	command := app.Command("disable", "Use default commit template and remove prepare-commit-msg hook")
-
-	return Definition{
-		CommandName: command.FullCommand(),
-		Policy: disable.Policy{
-			Deps: disable.Dependencies{
-				GitUnsetCommitTemplate: gitconfig.UnsetCommitTemplate,
-				GitUnsetHooksPath:      func() error { return gitconfig.UnsetAll("core.hooksPath") },
-				LoadConfig:             config.Load,
-				StatFile:               os.Stat,
-				RemoveFile:             os.Remove,
-				PersistDisabled:        staterepository.PersistDisabled,
-			},
+func policy() disable.Policy {
+	return disable.Policy{
+		Deps: disable.Dependencies{
+			GitUnsetCommitTemplate: gitconfig.UnsetCommitTemplate,
+			GitUnsetHooksPath:      func() error { return gitconfig.UnsetAll("core.hooksPath") },
+			LoadConfig:             config.Load,
+			StatFile:               os.Stat,
+			RemoveFile:             os.Remove,
+			PersistDisabled:        staterepository.PersistDisabled,
 		},
 	}
 }
