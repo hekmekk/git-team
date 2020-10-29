3e90ece9452a4c52222101e60e1ea2477121e799
Rea Sand <hekmek@posteo.de>
2020-10-29 08:29:32 +0100

    chore: remove dependency on kingpin

--- go.mod
+++ go.mod
@@ -3,14 +3,9 @@ module github.com/hekmekk/git-team
 go 1.12
 
 require (
-	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
-	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.7.0
 	github.com/mattn/go-colorable v0.1.1 // indirect
 	github.com/mattn/go-isatty v0.0.7 // indirect
-	github.com/stretchr/testify v1.3.0 // indirect
 	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/sys v0.0.0-20190422165155-953cdadca894 // indirect
-	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
--- src/command/adapter/adapter.go
+++ src/command/adapter/adapter.go
@@ -2,31 +2,15 @@ package commandadapter
 
 import (
 	"github.com/urfave/cli/v2"
-	"gopkg.in/alecthomas/kingpin.v2"
 
 	"github.com/hekmekk/git-team/src/core/effects"
 	"github.com/hekmekk/git-team/src/core/events"
 	"github.com/hekmekk/git-team/src/core/policy"
 )
 
-// CommandRoot kingpin application or command
-type CommandRoot interface {
-	Command(string, string) *kingpin.CmdClause
-}
-
 // RunUrFave apply a policy and convert the resulting event(s) to effects
 func RunUrFave(policy policy.Policy, adapter func(events.Event) []effects.Effect) func(c *cli.Context) error {
 	return func(c *cli.Context) error {
-		for _, effect := range adapter(policy.Apply()) {
-			effect.Run()
-		}
-		return nil
-	}
-}
-
-// Run apply a policy and convert the resulting event(s) to effects
-func Run(policy policy.Policy, adapter func(events.Event) []effects.Effect) func(c *kingpin.ParseContext) error {
-	return func(c *kingpin.ParseContext) error {
 		for _, effect := range adapter(policy.Apply()) {
 			effect.Run()
 		}
