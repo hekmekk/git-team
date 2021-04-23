feb43001b410f2a6a535f7d139d8b74202f98ef2
Rea Sand <hekmek@posteo.de>
2021-04-23 17:14:05 +0200

    fix: fail prepare-commit-msg hook when appropriate

--- git-hooks/prepare-commit-msg.sh
+++ git-hooks/prepare-commit-msg.sh
@@ -3,9 +3,17 @@
 REAL_LOCAL_HOOK="`git rev-parse --show-toplevel`/.git/hooks/`basename ${0}`"
 
 /usr/local/etc/git-team/hooks/prepare-commit-msg-git-team "$@"
+git_team_hook_result=$?
+if [ $git_team_hook_result -ne 0 ]; then
+    exit $git_team_hook_result
+fi
 
 if [ -f "${REAL_LOCAL_HOOK}" ]; then
    "${REAL_LOCAL_HOOK}" "$@"
+    local_hook_result=$?
+    if [ $local_hook_result -ne 0 ]; then
+        exit $local_hook_result
+    fi
 fi
 
 exit 0
