aa1bc0418baa4b964d9265c0cf8bb8fefc909b0d
Rea Sand <hekmek@posteo.de>
2021-04-23 17:16:40 +0200

    fix: fail proxied git hooks reliably

--- git-hooks/proxy.sh
+++ git-hooks/proxy.sh
@@ -3,7 +3,11 @@
 REAL_LOCAL_HOOK="`git rev-parse --show-toplevel`/.git/hooks/`basename ${0}`"
 
 if [ -f "${REAL_LOCAL_HOOK}" ]; then
-    exec "${REAL_LOCAL_HOOK}" "$@"
+    "${REAL_LOCAL_HOOK}" "$@"
+    result=$?
+    if [ $result -ne 0 ]; then
+        exit $result
+    fi
 fi
 
 exit 0
