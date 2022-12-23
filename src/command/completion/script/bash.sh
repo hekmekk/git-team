948d6b66a3575b11ae012513f014ecb328f2787d
Rea Sand <hekmek@posteo.de>
2022-12-23 17:21:11 +0100

    feat: WIP - bash completion for 'git-team' almost complete

--- src/command/completion/script/bash.sh
+++ src/command/completion/script/bash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # triggered for git team
 _git_team() {
-  echo "git team: COMP_CWORD=${COMP_CWORD} | COMP_CWORDS=${COMP_WORDS[@]}" >> /tmp/completion.log
   compopt +o default
   case ${COMP_WORDS[COMP_CWORD-1]} in
     -h | --help)
@@ -33,9 +32,8 @@ _git_team() {
 }
 # triggered for git-team
 _git_team_bash_completion() {
-  echo "git-team: COMP_CWORD=${COMP_CWORD} | COMP_CWORDS=${COMP_WORDS[@]}" >> /tmp/completion.log
   case ${COMP_WORDS[COMP_CWORD-1]} in
-    -h | --help)
+    -h | --help | -v | --version)
       COMPREPLY=()
       return
       ;;
@@ -44,18 +42,118 @@ _git_team_bash_completion() {
     1)
       case ${COMP_WORDS[COMP_CWORD]} in
         -*)
-          COMPREPLY=($(compgen -W "-h --help" -- "${COMP_WORDS[COMP_CWORD]}"))
+          COMPREPLY=($(compgen -W "-h --help -v --version" -- "${COMP_WORDS[COMP_CWORD]}"))
           ;;
         *)
-          COMPREPLY=($(compgen -W "first1 first2 first3" -- "${COMP_WORDS[COMP_CWORD]}"))
+          COMPREPLY=($(compgen -W "add assignments completion config disable enable list ls remove rm status help h" -- "${COMP_WORDS[COMP_CWORD]}"))
           ;;
       esac
       ;;
     2)
-      COMPREPLY=($(compgen -W "second1 second2 second3" -- "${COMP_WORDS[COMP_CWORD]}"))
+      case ${COMP_WORDS[COMP_CWORD-1]} in
+        add)
+          COMPREPLY=($(compgen -W "-f --force-override -k --keep-existing" -- "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        assignments)
+          COMPREPLY=($(compgen -W "add list ls remove rm help h" -- "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        completion)
+          COMPREPLY=($(compgen -W "bash zsh help h" -- "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        config)
+          COMPREPLY=($(compgen -W "activation-scope" -- "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        disable)
+          COMPREPLY=()
+          ;;
+        enable)
+          case ${COMP_WORDS[COMP_CWORD]} in
+            -*)
+              COMPREPLY=($(compgen -W "-A --all" -- "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+            *)
+              COMPREPLY=($(compgen -W "$(git team assignments list --only-alias)" --  "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+          esac
+          ;;
+        list | ls)
+          case ${COMP_WORDS[COMP_CWORD]} in
+            -*)
+              COMPREPLY=($(compgen -W "-o --only-alias" -- "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+            *)
+              COMPREPLY=()
+              ;;
+          esac
+          ;;
+        remove | rm)
+          COMPREPLY=($(compgen -W "$(git team assignments list --only-alias)" --  "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        status)
+          COMPREPLY=()
+          ;;
+        h | help)
+          COMPREPLY=($(compgen -W "add assignments completion config disable enable list ls remove rm status help h" -- "${COMP_WORDS[COMP_CWORD]}"))
+          ;;
+        *)
+          COMPREPLY=()
+          ;;
+      esac
+      ;;
+    3)
+      case ${COMP_WORDS[1]} in
+        assignments)
+          case ${COMP_WORDS[2]} in
+            add)
+              COMPREPLY=($(compgen -W "-f --force-override -k --keep-existing" -- "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+            list | ls)
+              case ${COMP_WORDS[COMP_CWORD]} in
+                -*)
+                  COMPREPLY=($(compgen -W "-o --only-alias" -- "${COMP_WORDS[COMP_CWORD]}"))
+                  ;;
+                *)
+                  COMPREPLY=()
+                  ;;
+              esac
+              ;;
+            remove | rm)
+              COMPREPLY=($(compgen -W "$(git team assignments list --only-alias)" --  "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+            *)
+              COMPREPLY=()
+              ;;
+          esac
+          ;;
+        config)
+          case ${COMP_WORDS[2]} in
+            activation-scope)
+              COMPREPLY=($(compgen -W "global repo-local" -- "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+            *)
+              COMPREPLY=()
+              ;;
+          esac
+          ;;
+      esac
       ;;
     *)
-      COMPREPLY=()
+      case ${COMP_WORDS[1]} in
+        enable)
+          case ${COMP_WORDS[2]} in
+            -*)
+              COMPREPLY=()
+              ;;
+            *)
+              # TODO: need to subsequently suggest the REMAINING aliases
+              COMPREPLY=($(compgen -W "$(git team assignments list --only-alias)" --  "${COMP_WORDS[COMP_CWORD]}"))
+              ;;
+          esac
+          ;;
+        *)
+          COMPREPLY=()
+          ;;
+      esac
       ;;
   esac
 }
